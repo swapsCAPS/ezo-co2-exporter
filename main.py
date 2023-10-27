bbe3b72ca74910b7cb3884fbcad3a194b62b0150
swapsCAPS <verstegen.daan@gmail.com>
2023-10-27 13:53:11 +0200

    spaces

--- main.py
+++ main.py
@@ -8,19 +8,19 @@ import string
 from serial import SerialException
 
 def send_cmd(cmd):
-	"""
-	Send command to the Atlas Sensor.
-	Before sending, add Carriage Return at the end of the command.
-	:param cmd:
-	:return:
-	"""
-	buf = cmd + "\r"     	# add carriage return
-	try:
-		ser.write(buf.encode('utf-8'))
-		return True
-	except SerialException as e:
-		print ("Error, ", e)
-		return None
+    """
+    Send command to the Atlas Sensor.
+    Before sending, add Carriage Return at the end of the command.
+    :param cmd:
+    :return:
+    """
+    buf = cmd + "\r"     	# add carriage return
+    try:
+        ser.write(buf.encode('utf-8'))
+        return True
+    except SerialException as e:
+        print ("Error, ", e)
+        return None
 
 port  = '/dev/ttyS0' # change to match your pi's setup
 delay = 1
@@ -28,10 +28,10 @@ gauge = Gauge('ezo_co2', 'EZO CO2 Readings')
 
 print( "Opening serial port...", port)
 try:
-	ser = serial.Serial(port, 9600, timeout=10000)
+    ser = serial.Serial(port, 9600, timeout=10000)
 except serial.SerialException as e:
-	print( "Could not open serial, ", e)
-	sys.exit(0)
+    print( "Could not open serial, ", e)
+    sys.exit(0)
 
 send_cmd("C,5") # turn on continuous mode
 #clear all previous data
@@ -43,18 +43,18 @@ print("Polling sensor every %0.2f seconds" % delay)
 start_http_server(8000)
 
 def read_lines():
-	try:
-		while True:
-			line = ser.read_until(b"\r", 12)
+    try:
+        while True:
+            line = ser.read_until(b"\r", 12)
 
-			co2 = int(line)
+            co2 = int(line)
             if (line.startsWith("*OK")):
-				continue
-			print(line)
-			print(co2)
+                continue
+            print(line)
+            print(co2)
 
-	except SerialException as e:
-		print( "Error, ", e)
-		return None
+    except SerialException as e:
+        print( "Error, ", e)
+        return None
 
 read_lines()
